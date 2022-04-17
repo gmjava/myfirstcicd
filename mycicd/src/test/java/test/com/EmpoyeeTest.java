c2cb5467b2085d0446452964124c63e18d0be4ac
gmjava <102937273+gmjava@users.noreply.github.com>
2022-04-16 22:54:34 -0500

    first project

---
+++ mycicd/src/test/java/test/com/EmpoyeeTest.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package test.com;
+
+import com.mycompany.mycicd.Employee;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author m_gab
+ */
+public class EmpoyeeTest {
+    Employee emp;
+    
+    public EmpoyeeTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void testFirst() {
+     emp = new Employee(1,"tom");
+     assertEquals(1, emp.getId());
+     assertEquals("tom", emp.getFirst());
+     
+     }
+}
